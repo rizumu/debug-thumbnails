f7966c87ad9d7a9fc6a708d2fbfd942b7ba09a25
Thomas Schreiber <tom@rizu.fake>
2014-01-05 21:23:45 +0100

    easy-thumbnail sucessfully stores thumbnail through cumulus

--- thumbnail/things/models.py
+++ thumbnail/things/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
+from cumulus.storage import SwiftclientStorage
+
+from easy_thumbnails.fields import ThumbnailerImageField
+from easy_thumbnails.alias import aliases
+
+
+if not aliases.get('thingthumb'):
+    aliases.set('thingthumb', {'size': (150, 80), 'crop': True})
+
 
 class Thing(models.Model):
     name = models.CharField(max_length=255, blank=True)
-    image = models.ImageField(upload_to="things")
+    image = ThumbnailerImageField(
+        upload_to='things', thumbnail_storage=SwiftclientStorage())
